1fc63d5575488fc3fd204d65e1f43004db8acd75
iluha2 <iluha2@e1.ru>
2023-10-04 14:25:18 +0300

    * GV_ExportToExcel is dead

--- src/Ex_GridUtils.pas
+++ src/Ex_GridUtils.pas
@@ -11,9 +11,9 @@ procedure GV_DrawStandartCell(Grid: TGridView; Cell: TGridCell; Canvas: TCanvas)
 procedure GV_DrawNoFocusCell(Grid: TGridView; Cell: TGridCell; Canvas: TCanvas);
 procedure GV_RegStorColumns(const Path: string; Grid: TGridView);
 procedure GV_RegRetrColumns(const Path: string; Grid: TGridView);
-{$IFDEF WINDOWS}
-function GV_ExportToExcel(GV: TCustomGridView; const Caption: String): Boolean; // NOTE !! not working !!
-{$ENDIF}
+//{$IFDEF WINDOWS}
+//function GV_ExportToExcel(GV: TCustomGridView; const Caption: String): Boolean; // NOTE !! not working !!
+//{$ENDIF}
 procedure GV_ExportToStrings(GV: TCustomGridView; const Caption: String; Ss: TStrings);
 
 
